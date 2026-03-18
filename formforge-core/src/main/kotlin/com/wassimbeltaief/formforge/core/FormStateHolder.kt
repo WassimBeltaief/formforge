380ae5a378679a5babf1b585ec416a562deb8efd
Wassim Beltaief <beltaief.wassim@gmail.com>
2026-03-18 22:10:36 +0100

    Add FormStateHolder interface

---
+++ formforge-core/src/main/kotlin/com/wassimbeltaief/formforge/core/FormStateHolder.kt
@@ -0,0 +1,13 @@
+package com.wassimbeltaief.formforge.core
+
+import com.wassimbeltaief.formforge.core.state.FormStatus
+import kotlinx.coroutines.flow.StateFlow
+
+public interface FormStateHolder<T : Any> {
+    public val data: T
+    public val status: StateFlow<FormStatus>
+    public fun validateAllSync(): Boolean
+    public fun reset()
+    public fun clear()
+    public fun setFieldError(field: String, error: String)
+}
